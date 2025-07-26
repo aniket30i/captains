814fa69a8a292e5f040d9e0e197bca96daf6934e
aniket30i <iamaniket.saha.09@protonmail.com>
2025-07-27 00:29:15 +0530

    component for net win by year graph

--- components/captains/Stats.jsx
+++ components/captains/Stats.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import WinLossLineCard from "../graphs/WinLossLineCard";
 import NetWinPosCard from "../graphs/NetWinPosCard";
-import VerticalBarChart from "../graphs/VerticalBarChart";
+import NetWinYearCard from "../graphs/NetWinYearCard";
 
 const Stats = () => {
   return (
@@ -10,20 +10,7 @@ const Stats = () => {
       <WinLossLineCard />
       <div className="grid grid-cols-2 h-[70%] gap-x-2 gap-y-1 p-1">
         <div className="bg-red-200 rounded-xl">1</div>
-        <div className="p-2 backdrop-blur-md bg-gray-900/35 shadow-2xl rounded-xl flex flex-col">
-          <p className="text-lg text-white font-gobold uppercase">
-            Net win by year
-          </p>
-          <div className="flex gap-4 font-gobold text-white text-sm">
-            <p>
-              <span className="text-2xl text-[#FAC515]">&#9679;</span> win
-            </p>
-            <p>
-              <span className="text-2xl text-[#FF0048]">&#9679;</span> losses
-            </p>
-          </div>
-          <VerticalBarChart />
-        </div>
+        <NetWinYearCard />
         <div className="bg-green-200 rounded-xl">3</div>
         <NetWinPosCard />
       </div>
---
+++ components/graphs/NetWinYearCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import VerticalBarChart from "../graphs/VerticalBarChart";
+
+const NetWinYearCard = () => {
+  return (
+    <div className="p-2 backdrop-blur-md bg-gray-900/35 shadow-2xl rounded-xl flex flex-col">
+      <p className="text-lg text-white font-gobold uppercase">
+        Net win by year
+      </p>
+      <div className="flex gap-4 font-gobold text-white text-sm">
+        <p>
+          <span className="text-2xl text-[#FAC515]">&#9679;</span> win
+        </p>
+        <p>
+          <span className="text-2xl text-[#FF0048]">&#9679;</span> losses
+        </p>
+      </div>
+      <VerticalBarChart />
+    </div>
+  );
+};
+
+export default NetWinYearCard;
